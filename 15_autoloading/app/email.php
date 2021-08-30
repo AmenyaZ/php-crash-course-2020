1b020c2293803f653dc04fda0db7a02ac6f80eb2
AmenyaZ <zamenya98@gmail.com>
2021-08-30 17:38:48 +0300

    json

---
+++ 15_autoloading/app/email.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app;
+class email
+{
+    public function __construct()
+    {
+        echo "email class ". '<br>';
+    }
+
+}
--- 15_autoloading/index.php
+++ 15_autoloading/index.php
@@ -1,1 +1,6 @@
 <?php
+require_once "app/email.php";
+require_once "app/person.php";
+
+$email = new app\Email();
+$person = new app\Person();
