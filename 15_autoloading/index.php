2406503021b29726d3f70136e9e30f3bc8b61759
AmenyaZ <zamenya98@gmail.com>
2021-08-30 17:59:14 +0300

    update

--- 15_autoloading/index.php
+++ 15_autoloading/index.php
@@ -1,6 +1,12 @@
 <?php
-require_once "app/email.php";
-require_once "app/person.php";
+require_once "vendor/autoload.php";
+//
+//$email = new app\email();
+//$person = new app\person();
+$client = new \GuzzleHttp\Client();
+$response = $client->request('GET', 'https://api.github.com/repos/guzzle/guzzle');
 
-$email = new app\Email();
-$person = new app\Person();
+echo $response->getStatusCode(); // 200
+echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+
